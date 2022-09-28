af59d05e21afbf610b9ba9eb9893e1e715ef9ed0
emily <nakanodepaula@gmail.com>
2022-09-28 20:19:17 -0300

    refactor: create folder to css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './assets/reset.css';
+
+import './assets/css/reset.css';
+import './assets/css/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
