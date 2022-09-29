29667a9574b5e77830c6263ba89e85588808a4df
emily <nakanodepaula@gmail.com>
2022-09-28 21:01:10 -0300

    feat: import font and utilize

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,22 +4,38 @@ import logo from '../assets/images/logo.png';
 export default function Header() {
   return (
     <Container>
-      <img src={logo} alt="" srcSet="" />
-      <h1>You will never forget!</h1>
+      <Main>
+        <img src={logo} alt="shinning" />
+        <h1>You will never forget!</h1>
+      </Main>
+      <Row />
     </Container>
-
   );
 }
 
 const Container = styled.div`
+`;
+
+const Row = styled.div`
+    height: 0px;
+    border: 1px solid #FBF3F3;
+`;
+
+const Main = styled.div`
     display: flex;
     flex-direction:column;
     align-items:center;
     justify-content:center;
 
-    height: 100px;
+    height: 85px;
     
     img {
         height: 50px;
+    }
+
+    h1 {
+        color: white;
+        font-weight: 700;
+        font-size: 16px;
     }
 `;
