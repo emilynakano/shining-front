6ec5ba400cddc1de8860bed8514f13c19ef57f10
emily <nakanodepaula@gmail.com>
2022-09-30 09:39:55 -0300

    feat: get data from inputs in sign-up

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,4 +1,6 @@
-import styled from 'styled-components';
+/* eslint-disable react/jsx-no-bind */
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 import Header from '../components/Header';
 import Input from '../components/form/Input';
@@ -7,34 +9,75 @@ import Title from '../components/form/Title';
 import Form from '../components/form';
 
 export default function SignUp() {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  function ChangeInput(e) {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
+
+  async function HandleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      console.log('signup');
+    } catch (error) {
+      toast.error(
+        'Invalid email, choose another one',
+      );
+    }
+  }
+
   return (
     <>
       <Header />
-      <Container>
+      <Form onSubmit={HandleSubmit}>
         <Title>
           <h1>Create an account</h1>
         </Title>
-        <Form>
-          <Input type="text" placeholder="Name" />
-          <Input type="text" placeholder="Email" />
-          <Input type="password" placeholder="Password" />
-          <Input type="password" placeholder="Confirm your password" />
-          <Button type="submit">
-            <span>Create Account</span>
-          </Button>
-        </Form>
-      </Container>
+        <Input
+          type="text"
+          placeholder="Name"
+          name="name"
+          required
+          value={user.name}
+          onChange={ChangeInput}
+        />
+        <Input
+          type="email"
+          placeholder="Email"
+          name="email"
+          required
+          value={user.email}
+          onChange={ChangeInput}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          name="password"
+          required
+          minLength={6}
+          value={user.password}
+          onChange={ChangeInput}
+        />
+        <Input
+          type="password"
+          placeholder="Confirm your password"
+          name="confirmPassword"
+          required
+          pattern={user.password}
+          value={user.confirmPassword}
+          onChange={ChangeInput}
+        />
+        <Button type="submit">
+          <span>Create Account</span>
+        </Button>
+      </Form>
     </>
 
   );
 }
-
-const Container = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items:center;
-
-  gap: 20px;
-
-  margin-top: 50px;
-`;
