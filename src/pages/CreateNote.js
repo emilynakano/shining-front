c2b9a92f7c6575c03b1d59675f5d451bca341d97
emily <nakanodepaula@gmail.com>
2022-10-01 22:35:58 -0300

    feat: use of editor

--- src/pages/CreateNote.js
+++ src/pages/CreateNote.js
@@ -1,10 +1,24 @@
+import MDEditor from '@uiw/react-md-editor';
+import { useState } from 'react';
+import styled from 'styled-components';
 import HeaderUser from '../components/HeaderUser';
 
 export default function CreateNote() {
+  const [value, setValue] = useState('');
   return (
     <>
       <HeaderUser />
-      <h1>oi</h1>
+      <Container>
+
+        <Editor>
+          <MDEditor height="60vh" value={value} onChange={setValue} />
+        </Editor>
+      </Container>
     </>
   );
 }
+const Editor = styled.div`
+  padding: 5%;
+`;
+const Container = styled.div`
+`;
