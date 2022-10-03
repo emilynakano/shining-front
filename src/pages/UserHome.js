df70b28b876ec59ef63e2225a809b014ec304b0a
emily <nakanodepaula@gmail.com>
2022-10-03 20:22:39 -0300

    feat: welcomes user

--- src/pages/UserHome.js
+++ src/pages/UserHome.js
@@ -3,9 +3,34 @@ import HeaderUser from '../components/HeaderUser';
 
 export default function UserHome() {
   return (
-    <HeaderUser />
+    <>
+      <HeaderUser />
+      <Container>
+        <Text>
+          <h2>Welcome, Emily!</h2>
+          <h2>Choose one of the options.</h2>
+        </Text>
+      </Container>
+    </>
   );
 }
 const Container = styled.div`
-  
+  padding-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Text = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  max-width: 420px;
+
+  gap: 10px;
+
+  font-size: 25px;
+  font-weight: 700;
+  color: white;
 `;
