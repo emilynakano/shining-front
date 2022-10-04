8d0172c540a61523aa53c9b54b750be8e9d7bf80
emily <nakanodepaula@gmail.com>
2022-10-03 21:09:00 -0300

    feat: title with 'notes'

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-unescaped-entities */
 import styled from 'styled-components';
 import HeaderUser from '../components/HeaderUser';
 
@@ -7,7 +6,9 @@ export default function Notes() {
     <>
       <HeaderUser />
       <Container>
-        notes
+        <Title>
+          <h2>NOTES</h2>
+        </Title>
       </Container>
     </>
   );
@@ -16,4 +17,21 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items:center;
+  h2 {
+    font-weight: 700;
+    color: white;
+  }
+`;
+const Title = styled.div`
+  width: 100%;
+  padding: 20px;
+  display: flex;
+  flex-direction: row;
+  align-items:center;
+  justify-content: space-between;
+  h2 {
+    font-weight: 700;
+    font-size: 20px;
+    color: white;
+  }
 `;
