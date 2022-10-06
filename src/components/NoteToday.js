0c1ca2b64b5e5df468449c6a6194bfe715db237f
emily <nakanodepaula@gmail.com>
2022-10-06 20:38:23 -0300

    feat: button to review, just layout

--- src/components/NoteToday.js
+++ src/components/NoteToday.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import MDEditor from '@uiw/react-md-editor';
 import Fade from 'react-reveal/Fade';
+import { AiFillCheckCircle } from 'react-icons/ai';
 
 export default function NoteToday({ note }) {
   const [click, setClick] = useState(false);
@@ -18,6 +19,7 @@ export default function NoteToday({ note }) {
 }
 
 function NoteContent({ content, click }) {
+  const [review, setReview] = useState(false);
   return (
     <Fade left opposite collapse when={click}>
       <Content>
@@ -25,6 +27,9 @@ function NoteContent({ content, click }) {
           source={content}
           linkTarget="_blank"
         />
+        <Icon review={review}>
+          <AiFillCheckCircle className="icon" />
+        </Icon>
       </Content>
     </Fade>
   );
@@ -34,9 +39,35 @@ const Content = styled.div`
     padding: 15px;
     background: white;
     border-radius: 5px;
+    .icon {
+      margin-top: 5px;
+      font-size: 35px;
+    }
+`;
+
+const Icon = styled.div`
+    display:flex;
+    flex-direction:column;
+    align-items:end;
+    .icon {
+      cursor:pointer;
+      color: ${(props) => (props.review ? 'green' : 'gray')};
+      font-size: 35px;
+      @keyframes go-back {
+      from {
+        transform: translateY(5px);
+      }
+      to {
+        transform: translateY(15px);
+      }
+      }
+      animation: go-back 1s infinite alternate;
+    }
 `;
 
 const Title = styled.div`
+  max-width:500px;
+  padding-left: 20px;
   white-space:nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
@@ -48,9 +79,11 @@ const Row = styled.div`
     box-shadow: 0px 1px 1px rgba(253, 253, 253, 0.1);
   `;
 const Container = styled.div` 
-    padding: 20px 20px;
     display: flex;
-    flex-direction: column;
+    flex-direction:row;
+    justify-content: space-between;
+    align-items:center;
+    height:55px;
     gap: 10px;
     h1 {
     color: white;
@@ -66,25 +99,4 @@ const Container = styled.div`
     transition: all .4s ease-in-out;
 }
 
-`;
-
-const DataNote = styled.div`
-    display: flex;
-    gap: 14px;
-`;
-
-const Date = styled.div`
-    h1 {
-    color: white;
-    font-size: 13px;
-    margin-top: 5px
-    }
-`;
-
-const Progress = styled.div`
-    h1 {
-    color: white;
-    font-size: 13px;
-    margin-top: 5px
-    }
 `;
