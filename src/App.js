4f109c4f88ebc6dee4092269d2458618719dd764
emily <nakanodepaula@gmail.com>
2022-10-06 16:59:53 -0300

    remove: create note in routers

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import 'react-toastify/dist/ReactToastify.min.css';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import Home from './pages/Home';
-import CreateNote from './pages/CreateNote';
 import UserHome from './pages/UserHome';
 import Notes from './pages/Notes';
 
@@ -18,7 +17,6 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/notes/create" element={<CreateNote />} />
         <Route path="/home" element={<UserHome />} />
         <Route path="/notes" element={<Notes />} />
       </Routes>
