ded521a12b68428d82572b1de6302696a32c76b2
emily <nakanodepaula@gmail.com>
2022-10-08 23:58:21 -0300

    feat: use of environment variables in api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://ec2-54-147-212-27.compute-1.amazonaws.com/',
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 export default api;
