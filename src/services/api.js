e5e984ea37d897df8d4e96d70546832b6908500c
emily <nakanodepaula@gmail.com>
2022-10-08 22:03:47 -0300

    feat: add url deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001',
+  baseURL: 'http://ec2-54-147-212-27.compute-1.amazonaws.com/',
 });
 
 export default api;
