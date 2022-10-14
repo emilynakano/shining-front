588dbd1c64b01f034f570d9a2cad1b49aa603586
emily <nakanodepaula@gmail.com>
2022-10-13 22:10:40 -0300

    feat: authprovider in app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import Routes from './routes';
+import { AuthProvider } from './hooks/useAuth';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <ToastContainer autoClose={3000} />
-      <Routes />
+      <AuthProvider>
+        <ToastContainer autoClose={3000} />
+        <Routes />
+      </AuthProvider>
     </BrowserRouter>
   );
 }
