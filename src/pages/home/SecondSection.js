5808510d8c3bd617e2cb5cd05fca7c8db3df0fbe
emily <nakanodepaula@gmail.com>
2022-10-22 18:31:42 -0300

    feat: title, description and image from herman in second section

--- src/pages/home/SecondSection.js
+++ src/pages/home/SecondSection.js
@@ -1,31 +1,70 @@
 import styled from 'styled-components';
-import { Title as StyleTitle, Description } from './FirstSection';
+import { Title as StyleTitle, Description as StyleDescription } from './FirstSection';
+import Hermann from '../../assets/images/Hermann.jpg';
 
 export default function SecondSection() {
   return (
-
     <Container>
-      <Title>
-        <h1>
-          How will we do for
-          you never forget?
-        </h1>
-      </Title>
-    </Container>
+      <Main>
+        <Title>
+          <h1>
+            How will we do for
+            you never forget?
+          </h1>
+        </Title>
+        <Description>
+          <h1>
+            That question who will answer
+            will be nothing more, nothing
+            less than the great scholar in
+            the area of human memory, he left
+            you a letter! Check out what Ebbinghaus
+            has to say!
+          </h1>
+        </Description>
+      </Main>
+      <LogoHermann>
+        <img src={Hermann} alt="herman" />
 
+      </LogoHermann>
+    </Container>
   );
 }
 const Container = styled.div`
   background: #FFFFFF;
   height: 100vh;
+
   display: flex;
+  flex-direction: row;
   justify-content:center;
-  text-align:center;
   padding-top: 80px;
 `;
+const Main = styled.div`
+  max-width: 520px;
+
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  text-align:center;
+  justify-content: space-around;
+  max-height: 310px;
+`;
 const Title = styled(StyleTitle)`
-  max-width: 500px;
   h1 {
     color:black;
+  }
+
+`;
+const Description = styled(StyleDescription)`
+  h1 {
+    color:black;
+  }
+
+`;
+const LogoHermann = styled.div`
+  img {
+    width: 300px;
+    height: 340px;
+    margin-right: 25px;
   }
 `;
