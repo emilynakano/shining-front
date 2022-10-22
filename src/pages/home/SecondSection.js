274a61dc0def50ce4980c770aa3af5e59b94f8b5
emily <nakanodepaula@gmail.com>
2022-10-22 16:40:31 -0300

    feat: add title from second section

--- src/pages/home/SecondSection.js
+++ src/pages/home/SecondSection.js
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
-import { Title, Description } from './FirstSection';
+import { Title as StyleTitle, Description } from './FirstSection';
 
 export default function SecondSection() {
   return (
 
     <Container>
       <Title>
-        <h1>It's not a </h1>
+        <h1>
+          How will we do for
+          you never forget?
+        </h1>
       </Title>
     </Container>
 
@@ -15,4 +18,14 @@ export default function SecondSection() {
 const Container = styled.div`
   background: #FFFFFF;
   height: 100vh;
+  display: flex;
+  justify-content:center;
+  text-align:center;
+  padding-top: 80px;
+`;
+const Title = styled(StyleTitle)`
+  max-width: 500px;
+  h1 {
+    color:black;
+  }
 `;
