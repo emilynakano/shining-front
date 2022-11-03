825ef9394a6f13d9a603ebb6eb3c83b162579b59
emily <nakanodepaula@gmail.com>
2022-11-03 16:15:40 -0300

    feat: forgetting curve section finished

--- src/pages/home/ForgettingCurveSection.js
+++ src/pages/home/ForgettingCurveSection.js
@@ -1,17 +1,105 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 import { Title as StyleTitle } from './FirstSection';
 import Curve from '../../assets/images/curve.png';
 
 export default function ForgettingCurveSection() {
+  const [click, setClick] = useState(false);
   return (
     <Container>
       <Title>
         <h1>The Forgetting Curve</h1>
       </Title>
-      <img src={Curve} alt="curve" />
+      <Main click={click}>
+        <div className="back-face" onClick={() => setClick(!click)}>
+          <img src={Curve} alt="curve" />
+        </div>
+        <div className="front-face" onClick={() => setClick(!click)}>
+          <h2>
+            The forgetting curve is the graphical representation of the
+            relationship between retention of acquired information and time
+            in which they remain in our memory. In this way, as
+            revisions are made the knowledge is more likely not to be lost.
+          </h2>
+        </div>
+      </Main>
     </Container>
   );
 }
+const Main = styled.div`
+  display: flex;
+  align-items:center;
+  justify-content:center;
+  width: 100%;
+  .back-face {
+    cursor: pointer;
+    max-width: 600px;
+    width: 90%;
+    max-height: 320px;
+
+    img {
+      width:100%;
+    }
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    position: relative;
+
+    transform: rotateY(180deg);
+    backface-visibility: hidden;
+    transition: all 1s;
+    transform-style: preserve-3d;
+
+    transform: ${(props) => (props.click ? '' : 'rotateY(0deg)')}
+  }
+  .front-face {
+    cursor: pointer;
+    max-width: 600px;
+    width: 80%;
+    padding:0 65px;
+    
+    height: 320px;
+
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    
+    position: absolute;
+
+    backface-visibility: hidden;
+    transition: all 1s;
+    transform-style: preserve-3d;
+
+    transform: ${(props) => (props.click ? '' : 'rotateY(-180deg)')};
+
+    h2 {
+      font-family: Roboto;
+      font-weight: 500;
+      font-size: 23px;
+      letter-spacing: 0.05em;
+      font-weight: 700px;
+      color: white;
+      text-align: start;
+    }
+    @media (max-width: 600px) {
+      padding:0px;
+    }
+    @media (max-width: 465px) {
+      h2 {
+        font-size: 17px;
+      }
+    }
+  }
+`;
+const Title = styled(StyleTitle)`
+ 
+  h1 {
+    color:white;
+  }
+`;
+
 const Container = styled.div`
   margin-top: 100px;
   display: flex;
@@ -19,15 +107,5 @@ const Container = styled.div`
   align-items:center;
   justify-content:center;
   text-align:center;
-  img {
-    margin-top: 50px;
-    width: 80%;
-    max-width: 600px;
-    cursor: pointer;
-  }
-`;
-const Title = styled(StyleTitle)`
-  h1 {
-    color:white;
-  }
+  gap:35px;
 `;
