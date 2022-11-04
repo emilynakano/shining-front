6c9cbaf8c1907eef7ea581ecbe93df03f8f0db06
emily <nakanodepaula@gmail.com>
2022-11-03 23:06:39 -0300

    feat: reasons to use this aplication

--- src/pages/home/MotivationSection.js
+++ src/pages/home/MotivationSection.js
@@ -1,13 +1,40 @@
 import styled from 'styled-components';
+import { FaFireAlt, FaClock, FaLightbulb } from 'react-icons/fa';
 import { Title as StyleTitle } from './FirstSection';
 
 export default function MotivationSection() {
   return (
     <Container>
-      <Title>
-        <h1>Why choose Shining platform?</h1>
-      </Title>
-      <Row />
+      <Main>
+        <Title>
+          <h1>Why choose Shining platform?</h1>
+        </Title>
+        <Row />
+        <Content>
+          <FaFireAlt size={28} />
+          <h1>
+            The only platform focused on memorization based
+            on Ebbinghaus theory.
+          </h1>
+        </Content>
+        <Row />
+        <Content>
+          <FaLightbulb size={28} />
+          <h1>
+            Creation of Markdown-style notes for review, allowing
+            for more complete editing.
+          </h1>
+        </Content>
+        <Row />
+        <Content>
+          <FaClock size={40} />
+          <h1>
+            Combat the forgetting curve with periodic reviews
+            (shortly after, one day, one week and one month later)
+          </h1>
+        </Content>
+        <Row />
+      </Main>
     </Container>
   );
 }
@@ -19,16 +46,39 @@ const Container = styled.div`
   align-items:center;
 `;
 
+const Main = styled.div`
+  width: 80%;
+  max-width: 600px;
+
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+`;
+
 const Row = styled.div`
+  width: 100%;
   margin: 18px 0;
   height: 1px;
   background-color:black;
-  width: 80%;
-  max-width: 600px;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  padding: 0 20px;
+  display: flex;
+  flex-direction: row;
+  gap: 15px;
+  align-items:center;
+  @media(max-width:600px) {
+    h1 {
+      font-size: 14px;
+    }
+    padding: 0 5px;
+  }
 `;
 
 const Title = styled(StyleTitle)`
-  max-width:600px;
+  width: 100%;
   text-align:center;
   h1 {
     color: black;
