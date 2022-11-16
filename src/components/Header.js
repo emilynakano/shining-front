095ca0142dd67e38d86d25fb2cbeb5681389a578
emily <nakanodepaula@gmail.com>
2022-11-16 10:48:41 -0300

    feat: navigate to the home page when click in the logo from sign-in and sign-up page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import logo from '../assets/images/logo.png';
 
 export default function Header() {
+  const navigate = useNavigate();
   return (
     <Container>
       <Main>
-        <img src={logo} alt="shinning" />
+        <img onClick={() => navigate('/')} src={logo} alt="shinning" />
         <h1>You will never forget!</h1>
       </Main>
       <Row />
@@ -32,6 +34,7 @@ const Main = styled.div`
     
     img {
         height: 50px;
+        cursor: pointer;
     }
 
     h1 {
