2dfe273e416d0c1b061299172c0645ba84d90e50
emily <nakanodepaula@gmail.com>
2022-11-17 09:46:55 -0300

    feat: initial layout static to plan page

---
+++ src/components/plan/Subscription.js
@@ -0,0 +1,80 @@
+import { AiOutlineRight } from 'react-icons/ai';
+import styled from 'styled-components';
+
+export default function Subscription() {
+  return (
+    <Container>
+      <h2>To sign</h2>
+      <Box>
+        <h2>Monthly signature</h2>
+        <h2>R$ 10.00/month</h2>
+        <div className="creditCard">
+          <div>
+            <h2>Credit Card</h2>
+            <AiOutlineRight color="white" size="50" />
+          </div>
+          <div>
+            <h2>Here will be the payment field</h2>
+          </div>
+        </div>
+      </Box>
+    </Container>
+  );
+}
+
+const Box = styled.div`
+    width: 100%;
+    max-width: 547px;
+
+    mix-blend-mode: normal;
+    border: 3px solid #FFFFFF;
+
+    padding: 20px;
+    
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    .creditCard {
+      cursor: pointer;
+
+      width: 100%;
+      margin-top: 10px;
+
+      border: 2px solid #8F8383;
+      padding-left: 10px;
+
+      display: flex;
+      flex-direction: column;
+      justify-content:Center;
+
+      > * {
+            &:first-child {
+                display: flex;
+                align-items:Center;
+                justify-content: space-between;
+                height: 89px;
+            }
+        }
+        > * {
+            &:nth-child(2) {
+                display: flex;
+                flex-direction: column;
+                display: none;
+            }
+        }
+    }
+`;
+
+const Container = styled.div`
+    width: 80%;
+    max-width: 545px;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    gap: 20px;
+    h2 {
+      font-size: 23px;
+      color: white;
+    }
+`;
--- src/pages/Plan.js
+++ src/pages/Plan.js
@@ -1,10 +1,87 @@
+import styled from 'styled-components';
 import Header from '../components/Header';
+import Subscription from '../components/plan/Subscription';
+import { Title as TitleStyle } from './home/FirstSection';
 
 export default function Plan() {
   return (
     <>
       <Header />
-      <h1>plan section</h1>
+      <Container>
+        <Title>
+          <h1>You chose</h1>
+        </Title>
+        <PlanContent>
+          <h2>Premium</h2>
+          <h2>R$ 10.00/month</h2>
+        </PlanContent>
+        <ChangePlan>
+          <h2>Change to free plan</h2>
+        </ChangePlan>
+        <Subscription />
+      </Container>
     </>
   );
 }
+const Title = styled(TitleStyle)`
+    margin-top: 50px;
+    text-align:center;
+    h1 {
+        font-size: 36px;
+        color: white;
+    }
+`;
+
+const Container = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content:center;
+
+    gap: 20px;
+
+    h2 {
+      font-weight: 800;
+      font-size: 25px;
+    }
+`;
+
+const ChangePlan = styled.div`
+    width: 80%;
+    max-width: 545px;
+    display: flex;
+    justify-content: end;
+    h2 {
+      cursor: pointer;
+      font-size: 20px;
+      color: white;
+    }
+`;
+
+const PlanContent = styled.div`
+    width: 80%;
+    max-width: 545px;
+    height: 98px;
+
+    background: #B92C2C;
+    border-radius: 0px 40px;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    padding: 20px;
+
+    h2 {
+      color: black;
+    }
+
+    @media(max-width: 400px) {
+      h2 {
+        font-size: 20px;
+      }
+    }
+   
+
+`;
